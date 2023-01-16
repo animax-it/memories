f3b724b04796918f719133d9187fc04e2d2889e0
animax-it <113898058+animax-it@users.noreply.github.com>
2023-01-17 02:43:44 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts'
+const url = 'https://memories-server-02ky.onrender.com/posts'
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost )
 export const updatePost = (id, updatedPost) => axios.put(`${url}/${id}`, updatedPost)
